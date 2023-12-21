0b79ac3c96a717e770bd51366a704ea9c76df64c
karpagam1602 <112971533+karpagam1602@users.noreply.github.com>
2023-12-21 18:04:13 +0530

    Day 5 JS files Committed.

---
+++ karpagam B -12110/Day - 5/JS demo/ex.js
@@ -0,0 +1,26 @@
+var x="kaviya";
+var y="20";
+var z="true";
+
+// dynamically typed --we can change the variable declaration
+
+x=20;
+
+
+document.write("<b> <u>" +"Hello All"+"</b></u>");
+
+document.write("<br>" +"THis is internal Js");
+document.writeln("<br>"+x);
+document.writeln("<br>"+y);
+document.writeln("<br>"+z);
+
+// == mean check only the value
+// ===check also the variable types
+
+document.writeln("<br>"+(x==y));
+document.writeln("<br>"+(x===y));
+
+
+// document.writeln("<br>"+x);
+// primitive-String,Boolean,Number
+// non-primitive-object
