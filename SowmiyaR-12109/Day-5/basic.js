ed204a961b42760d206daac5ec264f5617218fc4
Sowmiyar678 <153608788+Sowmiyar678@users.noreply.github.com>
2023-12-22 11:25:09 +0530

    Delete SowmiyaR-12109/Day-5 directory

--- SowmiyaR-12109/Day-5/basic.js
+++
@@ -1,4 +0,0 @@
-
-        document.write("<b><u>" +"Hello world"+"</br></u>")
-        document.write("<br>"+"This is my home page")
-    
--- SowmiyaR-12109/Day-5/variables.js
+++
@@ -1,7 +0,0 @@
-var x="sowmiya";
-var y=3;
-var c=true;
-x=4;
-document.write("<b><u>" +"Hello world"+"</br></u>");
-document.write("<br>"+"This is my home page "+(x===y));
-
