135a151d52d2521290959fc1423e4c05b5ebff37
sundhar raj <sundharachu59@gmail.com>
2023-12-25 21:32:38 +0530

    new features added

--- sundhar-12106/JavaScript/day2/ageCal.js
+++ sundhar-12106/JavaScript/day2/ageCal.js
@@ -1,8 +1,23 @@
 
 
-let date  = "2000-11-11"
+// let date  = "01-12-13";
+let form = document.getElementById("btn").addEventListener("click",e=>{
+   e.preventDefault();
+   // console.log("hello");
+   let date = document.getElementById("dob").value;
+   // console.log(date);
+   let ag = age(date)
+   if (isNaN(ag)) {
+      document.getElementById('res').innerHTML="please enter valid data";
+   } else {
+      document.getElementById('res').innerHTML="your age is : "+ag;  
+   }
+});
+// console.log(form);
+// form.
+
 function age(date) {
    return Math.floor((new Date() - new Date(date)) / 3.15576e+10);//
 
 }
-console.log(age(date));
+// console.log(age(date));
