473c914c0f283b8b8c02c9cb01b08ef2b1c21e5f
Sanjaykhanna <sanjaykhanna9122001@gmail.com>
2023-12-27 20:27:05 +0530

    Day 8 Task Completed and Commited by Sanjay Khanna

--- SanjayKhanna-12124/day7/BMICalculator/BMI.js
+++ SanjayKhanna-12124/day7/BMICalculator/BMI.js
@@ -1,12 +1,12 @@
 function cal(){
     var weight = parseFloat(document.getElementById("weight").value);
     var height = parseFloat(document.getElementById("height").value)/100;               //Converting into meter
-    if(weight==0||isNaN())
+    if(weight==0)
     alert("Weight cannot be Zero");
-    else if(height==0||isNaN())
+    else if(height==0)
     alert("height cannot be Zero");
     else{
-    var bmi = (weight/Math.pow(height,2));
-    document.getElementById("result").innerHTML="BMI Value : "+bmi+"<br><br>"+"<img src=\"https://img.freepik.com/premium-vector/man-body-mass-index-fitness-bmi-chart-with-male-silhouettes-scale_53562-7067.jpg?w=2000\" width =600px height= 400px>";
+    var bmi =(weight/Math.pow(height,2));
+    document.getElementById("result").innerHTML="BMI Value : "+bmi.toFixed(2)+"<br><br>"+"<img src=\"https://img.freepik.com/premium-vector/man-body-mass-index-fitness-bmi-chart-with-male-silhouettes-scale_53562-7067.jpg?w=2000\" width =600px height= 400px>";
     }
 } 
