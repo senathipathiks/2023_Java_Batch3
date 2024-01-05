13c0365afbd7b4684b22fc4a211033e3f10527bf
Sowmiyar678 <153608788+Sowmiyar678@users.noreply.github.com>
2024-01-05 09:58:39 +0530

    Day-11 files uploaded

---
+++ SowmiyaR-12109/Day-11 React js/Day-1/const.js
@@ -0,0 +1,24 @@
+const arr=[10,20,30];
+    
+// x=arr[0];
+// y=arr[1];
+const [first,second,third]=arr;
+console.log(first);
+let obj={
+    firstname:"Mages",
+    middlename:"Hari",
+    lastname:"Ram"
+}
+const{firstname:fname,middlename:mname,lastname:lname}=obj
+console.log(fname);
+
+
+    //     const obj={
+    //         name:"sowmi",
+    //         degree:"ph.d"
+    //     }
+    // console.log(obj.name);
+
+    //     obj.name="Hari"
+    //     console.log(obj.name);
+   
---
+++ SowmiyaR-12109/Day-11 React js/src/components/Day-1/FirstComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Mystyle.css"
+function FirstComponent() {
+  return (
+    <div>
+     <h1 align="center" class="fbg">WELCOME TO REACT.JS</h1> 
+    </div>
+  )
+}
+
+export default FirstComponent
