6c4f84f80c9492935d7c305f35b616016b368045
Deepika Suresh <97523191+Deepikavignesh@users.noreply.github.com>
2024-01-05 10:04:47 +0530

    React class Works updated

---
+++ Deepika-12137/React/js/destructuring.js
@@ -0,0 +1,14 @@
+const array=[1,2,3];
+x=array[0]
+y=array[1]
+// const [first,secound,third]=array;
+// console.log(first);
+
+let obj={
+    firstname:"Deepika",
+    secoundname:"sree",
+    thirdname:"Suresh"
+}
+
+const{firstname:fname,secoundname:mname,thirdname:lname}=obj
+console.log(fname);
