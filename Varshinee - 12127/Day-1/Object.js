9eee2ebcf7cd30d6042db9487ced48a6c797e16d
Varshinee <93853224+Varshineemeena@users.noreply.github.com>
2024-01-05 09:32:27 +0530

    Add files via upload

---
+++ Varshinee - 12127/Day-1/Object.js
@@ -0,0 +1,11 @@
+
+   
+        const obj={
+            name:"Arun",
+            degree:"ph.d"
+
+        }
+        console.log(obj.name)
+        obj.name="Kumar"
+        console.log(obj.name)
+  
---
+++ Varshinee - 12127/Day-1/mapex.js
@@ -0,0 +1,5 @@
+const arr=[10,20,30];
+const newarray=arr.map(function(element){
+    return element*3;
+    });
+    console.log(newarray);
