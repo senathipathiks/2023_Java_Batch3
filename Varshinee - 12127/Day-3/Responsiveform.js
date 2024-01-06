f8fb49c1b9e9fa05c0980aef88f5ed45e26394c2
Varshinee <93853224+Varshineemeena@users.noreply.github.com>
2024-01-06 17:56:20 +0530

    Update Responsiveform.js

--- Varshinee - 12127/Day-3/Responsiveform.js
+++ Varshinee - 12127/Day-3/Responsiveform.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react'
-import {TextField} from '@mui/material'
+import {TextField, button} from '@mui/material'
 import "./Mystyle.css";
 
-export class Responsiveform extends Component {
+export class ResponsiveForm extends Component {
   render() {
     return (
-        <center>
-        <div className="Responsiveform" align="center">
-       <div>
+        <center> 
+            <div class="Responsiveform"> 
+         <div className='container'>
+        <div className='form-group'>
+            <div className='row justify-content-center'>
+                <div className="col-12 col-md-10 col-xl-8">
+                    <div className='row justify-content-center'>
+                        <div className='col-lg-6 col-md-6 col-sm-6'>
     
   <TextField variant='outlined' label="Email" placeholder="Enter your email address" required /> <br/> <br/>
   <TextField variant="outlined" label="Password" placeholder='Enter your Password' required></TextField> <br/><br/>
   <TextField variant='outlined' label="Address" placeholder="Enter your current address" required /> <br/> <br/>
   <TextField variant='outlined' label="City" placeholder="City" required /> <br/> <br/>
-  <TextField variant='outlined' label="State" placeholder="City" required /> <br/> <br/>
+  <TextField variant='outlined' label="State" placeholder="Enter your state" required /> <br/> <br/>
   
    <button>SUBMIT</button>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
     </div>
     </div>
     </center>
@@ -24,4 +34,4 @@ export class Responsiveform extends Component {
   }
 }
 
-export default Responsiveform
+export default ResponsiveForm
