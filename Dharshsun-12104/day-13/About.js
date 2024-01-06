5d00b0464fbf091c8297ab79274208690f613088
Dharshsun. S <62539407+Dharshsun@users.noreply.github.com>
2024-01-06 21:24:32 +0530

    Create About.js

---
+++ Dharshsun-12104/day-13/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function About() {
+    return (
+        <div>
+            <h2>
+            We are in the business of custom software engineering.
+            </h2>
+            Read more about us at :
+            <a href="www.relevantz.com">
+            https://relevantz.com/about/
+            </a>
+        </div>
+    );
+}
+export default About;
