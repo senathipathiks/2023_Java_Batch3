b8b32b38a143ad41b3ffe2d779d5b64facd57f8c
Varshinee <93853224+Varshineemeena@users.noreply.github.com>
2024-01-10 09:18:21 +0530

    Task completed

--- Varshinee - 12127/src/index.js
+++ Varshinee - 12127/src/index.js
@@ -1,42 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Myapp from './components/Myapp';
-import Myapps from './components/Myapps';
-import Mylogin from './components/Mylogin';
-import MyClassapp from './components/MyClassapp';
-import Count from './components/Count';
-import Myfunccounter from './components/Myfunccounter';
-import Subscribe from './components/Subscribe';
-import Eventbind from './components/Eventbind';
-import Conditionalrend from './components/Conditionalrend';
-import ClassSubscribe from './components/ClassSubscribe';
-
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Myapp/>
-    <Myapp name="Arun" topic="React"></Myapp>
-    <p>Hello Alll</p>
-    <Myapps/>
-    <MyClassapp/>
-    <Mylogin/>
-    <Count/>
-    <Myfunccounter/>
-    <Subscribe/>
-    <Eventbind/>
-    <Conditionalrend/>
-    <ClassSubscribe/>
-    
-    
-    
-   
+    <App />
   </React.StrictMode>
 );
 
