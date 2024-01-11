c58e30b25799b195bbd8b8eb970e3fde009ece46
vigneshvellusamy1 <153538750+vigneshvellusamy1@users.noreply.github.com>
2024-01-11 22:23:10 +0530

    counter files

--- Vignesh V - 12108/src/App.js
+++ Vignesh V - 12108/src/App.js
@@ -1,98 +1,176 @@
-import React, { useState, useEffect } from "react";
-import PlayerList from "./components/PlayerList";
-import EditPlayer from "./components/EditPlayer";
-import AddPlayer from "./components/AddPlayer";
-import DeletePlayer from "./components/DeletePlayer";
-import data from "./data/players.json";
+//  import logo from './logo.svg';
+// import './App.css';
+// import { Provider } from 'react-redux';
+// import { store } from './Todocomponents/store';
+// //
+// // import { Container } from './counter/Container';
+// import Displaytodo from './Todocomponents/Displaytodo';
 
-function App() {
-  const [players, setPlayers] = useState(data.players);
-  const [currentPlayer, setCurrentPlayer] = useState(null);
-  const [showEditPlayer, setShowEditPlayer] = useState(false);
-  const [showAddPlayer, setShowAddPlayer] = useState(false);
-  const [showDeletePlayer, setShowDeletePlayer] = useState(false);
-  const [playerToDelete, setPlayerToDelete] = useState(null);
+// function App(){
+//   return(
+//     <div>
+//       <Provider store ={store}>
+//         <Displaytodo />
+//       </Provider>
+//     </div>
+//   );
+// }
+// export default App;
 
-  const handleDelete = (id) => {
-    setPlayerToDelete(id);
-    setShowDeletePlayer(true);
-  };
+// App.js File 
+import React, { Component } from "react"; 
+import "bootstrap/dist/css/bootstrap.css"; 
+import Container from "react-bootstrap/Container"; 
+import Row from "react-bootstrap/Row"; 
+import Col from "react-bootstrap/Col"; 
+import Button from "react-bootstrap/Button"; 
+import InputGroup from "react-bootstrap/InputGroup"; 
+import FormControl from "react-bootstrap/FormControl"; 
+import ListGroup from "react-bootstrap/ListGroup"; 
 
-  const deletePlayer = (id) => {
-    const updatedPlayers = players.filter((player) => player.id !== id);
-    setPlayers(updatedPlayers);
-    setShowDeletePlayer(false);
-  };
+class App extends Component { 
+	constructor(props) { 
+		super(props); 
 
-  const handleCancelDelete = () => {
-    setShowDeletePlayer(false);
-  };
+		// Setting up state 
+		this.state = { 
+			userInput: "", 
+			list: [], 
+		}; 
+	} 
 
-  const handleEdit = (id) => {
-    const player = players.find((player) => player.id === id);
-    setCurrentPlayer(player);
-    setShowEditPlayer(true);
-  };
+	// Set a user input value 
+	updateInput(value) { 
+		this.setState({ 
+			userInput: value, 
+		}); 
+	} 
 
-  const editPlayer = (updatedPlayer) => {
-    const updatedPlayers = players.map((player) =>
-      player.id === updatedPlayer.id ? updatedPlayer : player
-    );
-    setPlayers(updatedPlayers);
-    setCurrentPlayer(null);
-    setShowEditPlayer(false);
-  };
+	// Add item if user input in not empty 
+	addItem() { 
+		if (this.state.userInput !== "") { 
+			const userInput = { 
+				// Add a random id which is used to delete 
+				id: Math.random(), 
 
-  const handleCancelEdit = () => {
-    setCurrentPlayer(null);
-    setShowEditPlayer(false);
-  };
+				// Add a user value to list 
+				value: this.state.userInput, 
+			}; 
 
-  const handleAddPlayer = () => {
-    setShowAddPlayer(true);
-  };
+			// Update list 
+			const list = [...this.state.list]; 
+			list.push(userInput); 
 
-  const addPlayer = (player) => {
-    const newPlayer = {
-      id: Math.floor(Math.random() * 1000),
-      ...player,
-    };
-    const updatedPlayers = [...players, newPlayer];
-    setPlayers(updatedPlayers);
-    setShowAddPlayer(false);
-  };
+			// reset state 
+			this.setState({ 
+				list, 
+				userInput: "", 
+			}); 
+		} 
+	} 
 
-  const handleCancelAdd = () => {
-    setShowAddPlayer(false);
-  };
+	// Function to delete item from list use id to delete 
+	deleteItem(key) { 
+		const list = [...this.state.list]; 
 
-  return (
-    <div className="App">
-      <h1>Football Team</h1>
-      {showDeletePlayer ? (
-        <DeletePlayer
-          player={players.find((player) => player.id === playerToDelete)}
-          deletePlayer={deletePlayer}
-          onCancel={handleCancelDelete}
-        />
-      ) : showEditPlayer ? (
-        <EditPlayer
-          player={currentPlayer}
-          editPlayer={editPlayer}
-          onCancel={handleCancelEdit}
-        />
-      ) : showAddPlayer ? (
-        <AddPlayer addPlayer={addPlayer} onCancel={handleCancelAdd} />
-      ) : (
-        <PlayerList
-          players={players}
-          onDelete={handleDelete}
-          onEdit={handleEdit}
-          onAdd={handleAddPlayer}
-        />
-      )}
-    </div>
-  );
-}
+		// Filter values and leave value which we need to delete 
+		const updateList = list.filter((item) => item.id !== key); 
 
-export default App
+		// Update list in state 
+		this.setState({ 
+			list: updateList, 
+		}); 
+	} 
+
+	editItem = (index) => { 
+	const todos = [...this.state.list]; 
+	const editedTodo = prompt('Edit the todo:'); 
+	if (editedTodo !== null && editedTodo.trim() !== '') { 
+		let updatedTodos = [...todos] 
+		updatedTodos[index].value= editedTodo 
+		this.setState({ 
+		list: updatedTodos, 
+	}); 
+	} 
+	} 
+
+	render() { 
+		return ( 
+			<Container> 
+				<Row 
+					style={{ 
+						display: "flex", 
+						justifyContent: "center", 
+						alignItems: "center", 
+						fontSize: "3rem", 
+						fontWeight: "bolder", 
+					}} 
+				> 
+					TODO LIST 
+				</Row> 
+
+				<hr /> 
+				<Row> 
+					<Col md={{ span: 5, offset: 4 }}> 
+						<InputGroup className="mb-3"> 
+							<FormControl 
+								placeholder="add item . . . "
+								size="lg"
+								value={this.state.userInput} 
+								onChange={(item) => 
+									this.updateInput(item.target.value) 
+								} 
+								aria-label="add something"
+								aria-describedby="basic-addon2"
+							/> 
+							<InputGroup> 
+								<Button 
+									variant="dark"
+									className="mt-2"
+									onClick={() => this.addItem()} 
+								> 
+									ADD 
+								</Button> 
+							</InputGroup> 
+						</InputGroup> 
+					</Col> 
+				</Row> 
+				<Row> 
+					<Col md={{ span: 5, offset: 4 }}> 
+						<ListGroup> 
+							{/* map over and print items */} 
+							{this.state.list.map((item, index) => { 
+								return ( 
+								<div key = {index} > 
+									<ListGroup.Item 
+										variant="dark"
+										action 
+										style={{display:"flex", 
+												justifyContent:'space-between'
+									}} 
+									> 
+										{item.value} 
+										<span> 
+										<Button style={{marginRight:"10px"}} 
+										variant = "light"
+										onClick={() => this.deleteItem(item.id)}> 
+										Delete 
+										</Button> 
+										<Button variant = "light"
+										onClick={() => this.editItem(index)}> 
+										Edit 
+										</Button> 
+										</span> 
+									</ListGroup.Item> 
+								</div> 
+								); 
+							})} 
+						</ListGroup> 
+					</Col> 
+				</Row> 
+			</Container> 
+		); 
+	} 
+} 
+
+export default App; 
