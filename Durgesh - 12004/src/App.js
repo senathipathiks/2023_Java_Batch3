2d4c0959a6b46c09cec51fb28a8410a0be9db9dc
durgeshmurugan <153607234+durgeshmurugan@users.noreply.github.com>
2024-01-18 13:00:18 +0530

    Add files via upload

--- Durgesh - 12004/src/App.js
+++ Durgesh - 12004/src/App.js
@@ -1,31 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
+import { store } from './config-store';
+import { Container } from './counter/Container';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!!
-        </p>
-    <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider store={store}>
+        <Container />
+      </Provider>
     </div>
+  
   );
 }
 
-// function Hello() {
-//   return<h1>Hello World!</h1>
-// }
-
-
 export default App;
-
