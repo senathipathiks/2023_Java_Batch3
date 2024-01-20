5f0de27ee28d499ad8b12618c57208d1cbd8c8c2
karpagam1602 <112971533+karpagam1602@users.noreply.github.com>
2024-01-20 11:26:59 +0530

    MySQL files committed

---
+++ karpagam B -12110/mysql_db/ums_db_worker.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: ums_db
+-- ------------------------------------------------------
+-- Server version	8.0.35
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `worker`
+--
+
+DROP TABLE IF EXISTS `worker`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `worker` (
+  `WORKER_ID` int NOT NULL AUTO_INCREMENT,
+  `FIRST_NAME` char(25) DEFAULT NULL,
+  `LAST_NAME` char(25) DEFAULT NULL,
+  `SALARY` int DEFAULT NULL,
+  `JOINING_DATE` datetime DEFAULT NULL,
+  `DEPARTMENT` char(25) DEFAULT NULL,
+  PRIMARY KEY (`WORKER_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `worker`
+--
+
+LOCK TABLES `worker` WRITE;
+/*!40000 ALTER TABLE `worker` DISABLE KEYS */;
+INSERT INTO `worker` VALUES (1,'Monika','Arora',100000,'2021-02-20 09:00:00','HR'),(2,'Niharika','Verma',80000,'2021-06-11 09:00:00','Admin'),(3,'Vishal','Singhal',300000,'2021-02-20 09:00:00','HR'),(4,'Amitabh','Singh',500000,'2021-02-20 09:00:00','Admin'),(5,'Vivek','Bhati',500000,'2021-06-11 09:00:00','Admin'),(6,'Vipul','Diwan',200000,'2021-06-11 09:00:00','Account'),(7,'Satish','Kumar',75000,'2021-01-20 09:00:00','Account'),(8,'Geetika','Chauhan',90000,'2021-04-11 09:00:00','Admin');
+/*!40000 ALTER TABLE `worker` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-01-18 14:12:16
