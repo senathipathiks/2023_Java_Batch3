1ecd6172141e0c539db12aec16e462c1cd68e4b4
karpagam1602 <112971533+karpagam1602@users.noreply.github.com>
2024-01-24 09:07:25 +0530

    Java Day 3 F563.

---
+++ karpagam B -12110/Core_Java/Day3/StringEncryption.java
@@ -0,0 +1,27 @@
+package Com.Day3;
+
+public class StringEncryption {
+
+	public static void main(String[] args) {
+
+        String text = "Kaviya";
+        StringBuilder encryptedText = new StringBuilder();
+
+        for (char c : text.toCharArray()) {
+            if(Character.isLetter(c)) {
+                
+                int value = Character.toLowerCase(c) - 'a';
+                encryptedText.append(value).append(" ");
+            } else {
+                encryptedText.append(c);
+            }
+        }
+
+        System.out.println("Original Text: " + text);
+        System.out.println("Encrypted Text: " + encryptedText.toString());
+    }
+
+
+	}
+
+
