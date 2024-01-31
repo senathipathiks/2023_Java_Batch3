752535d4e418777f1241fa24a5f9233f5b94d4eb
Varshinee <93853224+Varshineemeena@users.noreply.github.com>
2024-01-31 16:35:51 +0530

    JDBC Uploaded

---
+++ Varshinee - 12127/sms/dao/StudentDAO.java
@@ -0,0 +1,60 @@
+package com.sms.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import com.sms.bean.Student;
+import com.sms.util.DBUtil;
+
+public class StudentDAO {
+
+public int insertStudent(Student stud) {
+		int n=0;
+		try {
+			Connection con=DBUtil.getDBConnection();
+			String sql="insert into student values(?,?,?)";
+			PreparedStatement ps=con.prepareStatement(sql);
+			ps.setInt(1, stud.getId());
+			ps.setString(2, stud.getName());
+			ps.setString(3, stud.getCity());
+			n=ps.executeUpdate();
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		return n;
+	}
+public int deleteStudent(int id) {
+	int n=0;
+	try {
+		Connection con=DBUtil.getDBConnection();
+		String sql="Delete from Student where id=?";
+		PreparedStatement ps=con.prepareStatement(sql);
+		ps.setInt(1, id);
+		n=ps.executeUpdate();
+	} catch(Exception e) {
+		System.out.println(e);
+	}
+	return n;
+}
+public int updateStudent(Student stud) {
+
+    int n = 0;
+    try {
+        Connection con = DBUtil.getDBConnection();
+        String sql = "update Students set name=? ,city=? where id=?";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setString(1, stud.getName());
+        ps.setString(2, stud.getCity());
+        ps.setInt(3, stud.getId());
+
+        n = ps.executeUpdate();
+    } catch (Exception e) {
+        System.out.println(e);
+    }
+    
+    return n;
+}
+
+}
---
+++ Varshinee - 12127/sms/main/SMSMain.java
@@ -0,0 +1,80 @@
+package com.sms.main;
+
+import java.util.Scanner;
+
+
+import com.sms.bean.Student;
+import com.sms.dao.StudentDAO;
+
+public class SMSMain {
+	static Scanner sc=new Scanner(System.in);
+	public static int displaymenu()
+	{
+		System.out.println("1.Insert 2.Delete 3.Update 4.Find 5.FindAll 6.Exit");
+		System.out.println("Enter your Choice");
+		int choice=sc.nextInt();
+		return choice;
+	}
+	
+	public static Student insertStudent()
+	{
+		System.out.println("Enter Student No, Student Name and Student City");
+		return new Student(sc.nextInt(),sc.next(),sc.next());
+	}
+	
+	public static int deleteStudent()
+	{
+		System.out.println("Enter the student id to delete");
+		return (sc.nextInt());
+	}
+	public static Student UpdateStudent()
+	{
+		System.out.println("Enter Student Id& Student Name & Student City");
+		return new Student(sc.nextInt(),sc.next(),sc.next());
+	}
+
+	public static void main(String[] args) {
+		String msg=" ";
+		int n;
+		StudentDAO dao=new StudentDAO();
+		do {
+			switch(displaymenu()) {
+			case 1:
+				Student st1=insertStudent();
+				n=dao.insertStudent(st1);
+				if(n==1)
+					System.out.println("Record Inserted Successfully");
+				else
+					System.out.println("Record Insertion Failure");
+				break;
+				
+			case 2:
+				int id=deleteStudent();
+				 n=dao.deleteStudent(id);
+	                if(n==1)
+	                    System.out.println("Record deleted successfully :)");
+	                else
+	                    System.out.println("Record Deletion failure");
+	                break;
+	        
+			case 3:
+				Student st2=UpdateStudent();
+				n=dao.updateStudent(st2);
+				if(n==1)
+					System.out.println("Record updated successfully");
+				else
+					System.out.println("record updation failure");
+				
+				
+			
+		}
+			System.out.println("Do you wish to continue(yes/no)");
+			msg=sc.next();
+	
+
+	}
+		while(msg.equals("yes"));
+	
+
+}}
+
