885e57acf447f5d8b39030bd68a5a1a57e1382b0
Balasubramanyam P <138239457+balasubramanayam@users.noreply.github.com>
2024-02-03 16:42:20 +0530

    Add files via upload

---
+++ Balasubramanyam - 12117/hotelroomreservationsystem/src/com/hrs/util/HRSUtil.java
@@ -0,0 +1,24 @@
+package com.hrs.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class HRSUtil {
+	public static Connection getDBConnection() 
+	{
+		Connection con=null;
+		final String URL="jdbc:mysql://localhost:3306/hrs";
+		final String User="root";
+		final String pass="root";
+				
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		
+			con=DriverManager.getConnection(URL,User,pass);
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+		return con;
+	}
+}
