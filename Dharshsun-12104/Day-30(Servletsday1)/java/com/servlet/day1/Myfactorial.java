7c87a3775861d74bf52d68bcfe4dc13f27e22cb3
Dharshsun. S <62539407+Dharshsun@users.noreply.github.com>
2024-02-06 11:00:00 +0530

    Update Myfactorial.java

--- Dharshsun-12104/Day-30(Servletsday1)/java/com/servlet/day1/Myfactorial.java
+++ Dharshsun-12104/Day-30(Servletsday1)/java/com/servlet/day1/Myfactorial.java
@@ -1,41 +1,36 @@
 package com.servlet.day1;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * Servlet implementation class Myfactorial
  */
 public class Myfactorial extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Myfactorial() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+			response.setContentType("text/html");
+			PrintWriter out = response.getWriter();
+			int num = Integer.parseInt(request.getParameter("text1"));
+			out.println("The factorial of the number is: ");
+			out.println(this.fact(num));
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-		
-	}
+			}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
+			long fact(long a) {
+			if (a <= 1)
+			return 1;
+			else {
+			a = a * fact(a - 1);
+			return a;
+			}
+			}
 }
+
+
