40ebd58f309b104388729d57d35aaf3bd4afbdbd
durgeshmurugan <153607234+durgeshmurugan@users.noreply.github.com>
2024-02-07 16:26:02 +0530

    Delete Durgesh - 12004/src directory

--- Durgesh - 12004/src/config-store.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-import {createStore} from 'redux'
-import { countReducer } from './counter/reducer.js';
-
-export const store = createStore(countReducer)
--- Durgesh - 12004/src/counter/Component.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Component = ({count, incrementClick, decrementClick}) => (
-    <div>
-        <h1>Hello React & Redux - Count: {count}</h1>
-        <button onClick={decrementClick}>Decrease</button>
-        <button onClick={incrementClick}>Increase</button>
-    </div>
-);
