afeaadca8df87f605caa19b372950775265d1af0
karpagam1602 <112971533+karpagam1602@users.noreply.github.com>
2024-02-28 15:27:48 +0530

    Spring Day 1 files Committed

---
+++ karpagam B -12110/Spring/SpringEg/src/main/java/com/day1/springExample/SpringEg/Bike.java
@@ -0,0 +1,10 @@
+package com.day1.springExample.SpringEg;
+
+public class Bike implements Vehicle {
+
+	@Override
+	public void run() {
+        System.out.println("This is the Bike class from the Vehicle Interface");
+	}
+
+}
