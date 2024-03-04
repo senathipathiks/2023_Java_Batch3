dedd6e09d916a0ad7aac0f359855ff72c212c2fb
Balasubramanyam <balasubramanip2002@gmail.com>
2024-03-04 20:31:57 +0530

    first commit

---
+++ Balasubramanyam B - 12117/ReactJS/Sample.java
@@ -0,0 +1,12 @@
+public class Sample{
+    public static void main(String[] args){
+        int x = 10; 
+        if (x > 5) {
+            System.out.println("x is greater than 5");
+        }
+        else {
+            System.out.println("x is not greater than 5");
+        }
+}
+
+}
---
+++ Balasubramanyam B - 12117/ReactJS/Sample1.java
@@ -0,0 +1,5 @@
+public class Sample1 {
+    public static void main(String[] args) {
+        System.out.println("Sample 1 file");
+    }
+}
