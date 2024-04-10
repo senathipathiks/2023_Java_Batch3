148d638333dd394b0b2ebe303de7c242767eb419
jo <105811806+JOTHI3117@users.noreply.github.com>
2024-04-10 17:04:03 +0530

    Add files via upload

---
+++ JOTHILINGAM A -12114/hibernate/HQLProgram/src/main/java/com/sts/day1/Employee/Employee.java
@@ -0,0 +1,57 @@
+package com.sts.day1.Employee;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="EmpDetail")
+public class Employee {
+	@Id
+	private int eid;
+	@Column(name="name")
+	private String ename;
+	@Column(name="dept")
+	private int dept;
+	
+	public Employee() {
+		super();
+	}
+
+	public Employee(int eid, String ename, int dept) {
+		super();
+		this.eid = eid;
+		this.ename = ename;
+		this.dept = dept;
+	}
+
+	public int getEid() {
+		return eid;
+	}
+
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public int getDept() {
+		return dept;
+	}
+
+	public void setDept(int dept) {
+		this.dept = dept;
+	}
+
+	@Override
+	public String toString() {
+		return "eid=" + eid + ", ename=" + ename + ", dept=" + dept;
+	}
+	
+	
+
+}
