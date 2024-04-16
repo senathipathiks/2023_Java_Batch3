539ecd529aad055b2f35a09f2a38cf8a64690967
karpagam1602 <112971533+karpagam1602@users.noreply.github.com>
2024-04-16 16:43:13 +0530

    Junit files Committed

---
+++ karpagam B -12110/Testing - Junit/Day 1/junitsuite/firstpack/Test2.java
@@ -0,0 +1,14 @@
+package com.junitsuite.firstpack;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class Test2 {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
